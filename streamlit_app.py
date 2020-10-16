58ec99870cc22f68fd66a032aefba6f5035fa9eb
Xinyi Xu <amandaxu98@gmail.com>
2020-10-15 23:18:14 -0700

    basic data exploration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
 import streamlit as st
+import numpy as np
 import pandas as pd
 import altair as alt
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+@st.cache(persist=True)
+def load_data(path):
+	weather=pd.read_csv(path)
+	weather['date']=weather['date'].astype('str')
+	weather['date']= pd.to_datetime(weather['date'], format='%Y%m%d')
+	weather['month'] = pd.DatetimeIndex(weather['date']).month
+	weather = weather.rename(columns={"PRCP": "Precipitation (inch)", "SNOW": "Snowfall (inch)", "SNWD": "Snow depth (inch)", "TMAX": "Maximum temperature (F)","TMIN": "Minimum temperature (F)", "TAVG": "Average temperature (F)","AWND": "Average daily wind speed (miles / hour)", "WSF5": "Fastest 5-second wind speed (miles / hour)", "WDF5": "Direction of fastest 5-second wind (degrees)"})
+	return weather
 
-@st.cache  # add caching so we load the data only once
-def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+weather=load_data("weather.csv")
 
-df = load_data()
+#part1: a table with average temperature, windspeed snow, precipitation elevation, for each state, cna choose time range
+st.title("Average measurement for each state")
 
-st.write("Let's look at raw data in the Pandas Data Frame.")
+table_weather=weather[['state','elevation','Average temperature (F)','Average daily wind speed (miles / hour)','Snowfall (inch)', "Snow depth (inch)",'Precipitation (inch)','month']]
+table_weather=table_weather.dropna(subset=['month'])
+table_weather['month'] = table_weather['month'].astype(int) 
 
-st.write(df)
+#choose measurement
+cols = ['state','elevation','Average temperature (F)','Average daily wind speed (miles / hour)', "Snow depth (inch)",'Snowfall (inch)','Precipitation (inch)']
+st_ms = st.multiselect("Please select measurements of your interest", table_weather.columns.tolist(), default=cols)
 
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
+#choose month
+months = st.slider("Please select range of month of your interest", min(table_weather.month), max(table_weather.month), (min(table_weather.month),max(table_weather.month)))
+table_weather=table_weather[(table_weather['month']<=months[1]) & (table_weather['month']>=months[0])]
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
+#choose state
+states = table_weather['state'].unique()
+st_state = st.multiselect("Please select state of your interest", states)
 
-st.write(chart)
+table_weather=table_weather[st_ms]
+
+mask_states = table_weather['state'].isin(st_state)
+table_weather = table_weather[mask_states]
+
+st.table(table_weather.groupby("state").mean())
+
+
+#part 2: a lineplot with average over 12 months
+st.title("Average measurement for each month")
+
+table_month=weather[['Average temperature (F)','Minimum temperature (F)','Maximum temperature (F)',\
+'Average daily wind speed (miles / hour)','Snowfall (inch)', "Snow depth (inch)",'Precipitation (inch)',\
+'Fastest 5-second wind speed (miles / hour)','Direction of fastest 5-second wind (degrees)','month']]
+table_month=table_month.dropna(subset=['month'])
+table_month['month'] = table_month['month'].astype(int) 
+
+#choose measurement
+option = st.selectbox(
+    'Which measurement are you interested in for month?',
+     ['Average temperature (F)','Minimum temperature (F)','Maximum temperature (F)',\
+'Average daily wind speed (miles / hour)','Snowfall (inch)', "Snow depth (inch)",'Precipitation (inch)',\
+'Fastest 5-second wind speed (miles / hour)','Direction of fastest 5-second wind (degrees)'])
+
+table_month = table_month[[option,"month"]].groupby("month").mean().reset_index()
+
+line = alt.Chart(table_month).mark_line().encode(    
+	alt.Y(option, scale=alt.Scale(zero=False)),    
+	alt.X("month", scale=alt.Scale(zero=False)),    
+)
+
+st.write(line)
+
+
+#part 3 average measurement for different elevation
+st.title("Average measurement for different elevation")
+
+table_elevation=weather[['Average temperature (F)','Minimum temperature (F)','Maximum temperature (F)',\
+'Average daily wind speed (miles / hour)','Snowfall (inch)', "Snow depth (inch)",'Precipitation (inch)',\
+'Fastest 5-second wind speed (miles / hour)','Direction of fastest 5-second wind (degrees)','elevation']]
+table_elevation=table_elevation.dropna(subset=['elevation'])
+
+#choose measurement
+option = st.selectbox(
+    'Which measurement are you interested in for elevation?',
+     ['Average temperature (F)','Minimum temperature (F)','Maximum temperature (F)',\
+'Average daily wind speed (miles / hour)','Snowfall (inch)', "Snow depth (inch)",'Precipitation (inch)',\
+'Fastest 5-second wind speed (miles / hour)','Direction of fastest 5-second wind (degrees)'])
+
+table_elevation = table_elevation[[option,"elevation"]]
+
+table_elevation=table_elevation.groupby(pd.cut(table_elevation["elevation"], np.arange(min(table_elevation["elevation"]), max(table_elevation["elevation"]), 500))).mean().reset_index(drop=True)
+table_elevation['elevation']=(table_elevation['elevation']//500*500).astype(int).astype(str)+"-"+(table_elevation['elevation']//500*500+500).astype(int).astype(str)
+bar = alt.Chart(table_elevation).mark_bar().encode(    
+	alt.Y(option, scale=alt.Scale(zero=False)),    
+	alt.X("elevation", scale=alt.Scale(zero=False)),    
+).properties(width=600,height=400)
+
+st.write(bar)
